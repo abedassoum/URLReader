99470648635c599e3d627108cfdc229bdc75485e
Abed Rass <75977129+abedassoum@users.noreply.github.com>
2024-02-11 13:59:55 +0100

    Deleted tag

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        tag t = new tag();
 
         int count = WordCounter.countWords("https://www.google.com", "meta");
         System.out.println(count);
